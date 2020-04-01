550e8e06ae09aac444f0ed88823c9deb7f4123b9
Scott Lavine <salavine1@hotmail.com>
2020-04-01 05:52:13 -0500

    My Initial Build

---
+++ SermonCodeGenerator/SermonCodeGenerator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SermonCodeGenerator
+//
+//  Created by Scott Lavine on 4/1/20.
+//  Copyright © 2020 Scott Lavine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
